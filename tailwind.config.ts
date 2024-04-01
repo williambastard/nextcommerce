3d407869d2d49a6fd93813272292ca2a04d247b5
William BASTARD <65223866+williambastard@users.noreply.github.com>
2024-04-01 23:44:09 +0200

    update

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -12,9 +12,13 @@ const config: Config = {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      }, gridTemplateRows: {
+        '[auto,auto,1fr]': 'auto auto 1fr',
       },
     },
   },
-  plugins: [],
+  plugins: [
+    require('@tailwindcss/aspect-ratio'),
+  ],
 };
 export default config;
